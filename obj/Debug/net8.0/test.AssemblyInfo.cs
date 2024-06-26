127d998ec5c2cd5662d4aecd5787848ddf46b019
zpdh <36316038+zpdh@users.noreply.github.com>
2024-06-25 23:29:39 -0300

    Moved files to different folder

--- obj/Debug/net8.0/test.AssemblyInfo.cs
+++ obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d797c4cbefa8a461e849b52d9ec433a934317399")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ef370e0447a27ff8ac4304735bb56239412663a")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
