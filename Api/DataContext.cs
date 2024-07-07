7ff6024ed67823afe033e3ca895917690cf897d2
zpdh <36316038+zpdh@users.noreply.github.com>
2024-07-07 12:56:06 -0300

    Remade entire api; hopefully its working now

---
+++ Api/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using test.Services;
+
+namespace test.Api;
+
+public class DataContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    public DataContext() {}
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseMySQL(ConfigurationManager.AppSettings["ConnectionString"]);
+        }
+    }
+}
---
+++ Api/TomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using test.Services;
+
+namespace test.Api;
+
+public class TomeController
+{
+    private readonly DataContext _dataContext = new DataContext();
+
+    public async Task<IEnumerable<User>> GetTomeQueueAsync()
+    {
+        var userList = await _dataContext.Users.AsNoTracking().ToListAsync();
+        return userList;
+    }
+
+    public async Task<string> RemovePlayerAsync(User user)
+    {
+        var dbUser = await _dataContext.Users.FirstOrDefaultAsync(u => u.UserName.ToUpper() == user.UserName.ToUpper());
+        if (dbUser == null) return "User not found";
+        
+        _dataContext.Users.Remove(user);
+        await _dataContext.SaveChangesAsync();
+        
+        return "Successfully removed player from tome list";
+    }
+
+    public async Task<string> AddPlayerAsync(User user)
+    {
+        var dbUser = await _dataContext.Users.FirstOrDefaultAsync(u => u.UserName.ToUpper() == user.UserName.ToUpper());
+        if (dbUser != null) return "User is already in list";
+        
+        await _dataContext.Users.AddAsync(user);
+        await _dataContext.SaveChangesAsync();
+        
+        return "Successfully added player to tome list";
+    }
+}
