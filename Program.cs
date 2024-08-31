1aed91c4ad87d7bd5061eaae2c42f8d1eedaf647
Antunes <36316038+zpdh@users.noreply.github.com>
2024-08-31 13:59:31 -0300

    chore: remove redundant loop

--- Program.cs
+++ Program.cs
@@ -11,24 +11,7 @@ public static class Program
         var discordClient = await DiscordAppBuilder.SetupDiscordClientAsync(config);
 
         discordClient.AddCommands(config);
-
-
-        // Keeps the bot running. If WebSocketException is thrown
-        // (discord disconnects bot) simply logs on the console and
-        // reconnects automatically.
-        // If any exceptional exception is thrown, console log it for
-        // debugging.
-        while (true)
-        {
-            try
-            {
+        
                 await Task.Delay(-1);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("An exception occurred: " + e.Message);
-            }
-        }
-        // ReSharper disable once FunctionNeverReturns
     }
 }
