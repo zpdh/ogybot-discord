e04e6ab98cb81f19291f6cb3ae09217272951136
Antunes <36316038+zpdh@users.noreply.github.com>
2024-09-02 20:53:16 -0300

    docs: add comments to Program.cs

--- Program.cs
+++ Program.cs
@@ -7,11 +7,13 @@ public static class Program
 {
     public static async Task Main()
     {
+        // Instance and setup IConfiguration and DiscordSocketClient
         var config = AppConfigurationBuilder.Build();
         var discordClient = await DiscordAppBuilder.SetupDiscordClientAsync(config);
 
         discordClient.AddCommands(config);
 
+        // Delay the task until program is closed
         await Task.Delay(-1);
     }
 }
