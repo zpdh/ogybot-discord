4b406000ce28e74825ecebc60c8c763ca8eafcfc
Antunes <36316038+zpdh@users.noreply.github.com>
2024-09-04 05:04:24 -0300

    fix: fix invalid channel constant

--- Util/GuildChannels.cs
+++ Util/GuildChannels.cs
@@ -4,7 +4,7 @@ public abstract class GuildChannels
 {
     public const ulong TomeChannel = 1125517737188409364;
     public const ulong LayoffsChannel = 1135296640803147806;
-    public const ulong RaidsChannel = 1255506035091968010;
+    public const ulong RaidsChannel = 1272044811771449365;
     public const ulong WarQuestionsChannel = 1255011451056423013;
     public const ulong WarChannel = 863553410813001759;
 }
