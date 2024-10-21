8f8d0af9face4bb1921848fb1372f4992cddc979
Antunes <36316038+zpdh@users.noreply.github.com>
2024-10-21 02:27:50 -0300

    refactor: refactor WaitlistCommand.cs

--- Commands/Waitlist/WaitlistCommand.cs
+++ Commands/Waitlist/WaitlistCommand.cs
@@ -2,6 +2,7 @@
 using Discord.Interactions;
 using Discord.WebSocket;
 using ogybot.DataAccess.Controllers;
+using ogybot.DataAccess.Entities;
 using ogybot.Util;
 
 namespace ogybot.Commands.Waitlist;
@@ -24,13 +25,43 @@ public class WaitlistCommand : BaseCommand
     {
         if (await IsInvalidChannelAsync(GuildChannels.LayoffsChannel)) return;
 
+        var embed = await CreateEmbedAsync();
+
+        await FollowupAsync(embed: embed);
+    }
+
+    private async Task<Embed> CreateEmbedAsync()
+    {
+        // Create class to store this info later
+        var (user, queueSize, description) = await GetEmbedContentAsync();
+
+        var embedBuilder = new EmbedBuilder()
+            .WithAuthor(user.Username, user.GetAvatarUrl() ?? user.GetDefaultAvatarUrl())
+            .WithTitle("Wait list")
+            .WithDescription(description)
+            .WithColor(Color.Teal)
+            .WithCurrentTimestamp()
+            .WithFooter(queueSize);
+
+        return embedBuilder.Build();
+    }
+
+    private async Task<(SocketUser user, string queueSize, string description)> GetEmbedContentAsync()
+    {
         var user = Context.User;
 
         var list = await _controller.GetWaitlistAsync();
 
-        var description = "";
-
         var queueSize = "Players in queue: " + list.Count;
+
+        var description = CreateEmbedDescription(list);
+
+        return (user, queueSize, description);
+    }
+
+    private static string CreateEmbedDescription(List<UserWaitlist> list)
+    {
+        var description = "";
 
         var counter = 1;
 
@@ -41,21 +72,6 @@ public class WaitlistCommand : BaseCommand
             counter++;
         }
 
-        var embed = CreateEmbed(user, description, queueSize);
-
-        await FollowupAsync(embed: embed);
-    }
-
-    private static Embed CreateEmbed(SocketUser user, string description, string queueSize)
-    {
-        var embedBuilder = new EmbedBuilder()
-            .WithAuthor(user.Username, user.GetAvatarUrl() ?? user.GetDefaultAvatarUrl())
-            .WithTitle("Wait list")
-            .WithDescription(description)
-            .WithColor(Color.Teal)
-            .WithCurrentTimestamp()
-            .WithFooter(queueSize);
-
-        return embedBuilder.Build();
+        return description;
     }
 }
