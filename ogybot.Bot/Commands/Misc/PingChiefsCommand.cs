556069ae0f2aa3e18ffd268edee3ba0d64fe6d43
Antunes <36316038+zpdh@users.noreply.github.com>
2024-10-23 20:22:52 -0300

    feat: add ping chiefs command

---
+++ ogybot.Bot/Commands/Misc/PingChiefsCommand.cs
@@ -0,0 +1,21 @@
+﻿using Discord;
+using Discord.Interactions;
+using ogybot.Bot.Commands.Base;
+using ogybot.Communication.Constants;
+
+namespace ogybot.Bot.Commands.Misc;
+
+public class PingChiefsCommand : BaseCommand
+{
+    [CommandContextType(InteractionContextType.Guild)]
+    [SlashCommand("chiefs", "Pings the 'Active Chief' role")]
+    public async Task ExecuteChiefsCommandAsync()
+    {
+        if (await IsInvalidChannelAsync(GuildChannels.WarChannel))
+        {
+            return;
+        }
+
+        await FollowupAsync($"<@&{GuildRoleIds.ActiveChiefRole}>", allowedMentions: AllowedMentions.All);
+    }
+}
