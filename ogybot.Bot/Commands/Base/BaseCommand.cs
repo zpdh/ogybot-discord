88105e1c8646f09f3ddf6c4c191b5c8c41051d82
Antunes <36316038+zpdh@users.noreply.github.com>
2024-10-23 19:59:13 -0300

    feat: start implementing commands

---
+++ ogybot.Bot/Commands/Base/BaseCommand.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Interactions;
+using ogybot.Communication.Constants;
+
+namespace ogybot.Bot.Commands.Base;
+
+public abstract class BaseCommand : InteractionModuleBase<SocketInteractionContext>
+{
+    protected async Task<bool> IsInvalidChannelAsync(ulong channelId)
+    {
+        if (Context.Channel.Id == channelId) return false;
+
+        await FollowupAsync(ErrorMessages.InvalidChannelError);
+        return true;
+    }
+}
---
+++ ogybot.Bot/Commands/Base/BasePermissionRequiredCommand.cs
@@ -0,0 +1,44 @@
+﻿using Discord;
+using ogybot.Communication.Constants;
+
+namespace ogybot.Bot.Commands.Base;
+
+public class BasePermissionRequiredCommand : BaseCommand
+{
+    /// <summary>
+    /// Validates the user and channel the message has been sent to determine whether the context is valid or not.
+    /// </summary>
+    /// <param name="channelId"></param>
+    /// <returns></returns>
+    protected async Task<bool> IsInvalidContextAsync(ulong channelId)
+    {
+        return await ValidateChannelAndRolesAsync(channelId);
+    }
+
+    private async Task<bool> ValidateChannelAndRolesAsync(ulong channelId)
+    {
+
+        if (await IsInvalidChannelAsync(channelId)) return true;
+
+        return await ValidateUserRolesAsync();
+
+    }
+
+    private async Task<bool> ValidateUserRolesAsync()
+    {
+
+        var user = Context.User as IGuildUser;
+
+        var roles = user!
+            .RoleIds
+            .Where(role => role is GuildRoleIds.AdminRole
+                or GuildRoleIds.ChiefRole
+                or GuildRoleIds.DeveloperRole);
+
+        if (roles.Any()) return false;
+
+        await FollowupAsync(ErrorMessages.NoPermissionError);
+
+        return true;
+    }
+}
