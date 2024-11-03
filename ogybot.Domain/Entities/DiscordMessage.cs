bda111bb1b09c93351fd810ae3ff50d522f5aa34
Antunes <36316038+zpdh@users.noreply.github.com>
2024-11-03 09:00:54 -0300

    feat: continue ChatSocket.cs implementation

---
+++ ogybot.Domain/Entities/DiscordMessage.cs
@@ -0,0 +1,17 @@
+﻿namespace ogybot.Domain.Entities;
+
+public class DiscordMessage
+{
+    public string Author { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+
+    public DiscordMessage()
+    {
+    }
+
+    public DiscordMessage(string author, string content)
+    {
+        Author = author;
+        Content = content;
+    }
+}
