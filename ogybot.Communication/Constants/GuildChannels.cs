09238c9ef8e540f4f6f653e4d6c7a7738044c556
Antunes <36316038+zpdh@users.noreply.github.com>
2024-11-25 16:28:49 -0300

    chore: input correct channel ids

--- ogybot.Communication/Constants/GuildChannels.cs
+++ ogybot.Communication/Constants/GuildChannels.cs
@@ -5,7 +5,6 @@ public static class GuildChannels
     public const ulong TomeChannel = 1125517737188409364;
     public const ulong LayoffsChannel = 1135296640803147806;
     public const ulong RaidsChannel = 1272044811771449365;
-    public const ulong WarQuestionsChannel = 1255011451056423013;
     public const ulong WarChannel = 863553410813001759;
-    public const ulong WebsocketLogChannel = 1255506035091968010;
+    public const ulong WebsocketLogChannel = 1290068270963232868;
 }
