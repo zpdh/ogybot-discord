360c27afcc99ec774cd4e2c1f66dd9f9d109c3e0
Antunes <36316038+zpdh@users.noreply.github.com>
2024-12-27 04:04:24 -0300

    feat: fix GuildClient.cs endpoint route being incorrect

--- ogybot.Infrastructure/Clients/GuildClient.cs
+++ ogybot.Infrastructure/Clients/GuildClient.cs
@@ -6,7 +6,7 @@ namespace ogybot.Data.Clients;
 
 public sealed class GuildClient : BaseClient, IGuildClient
 {
-    private const string Endpoint = "guilds/config";
+    private const string Endpoint = "config";
 
     private readonly ITokenRequester _tokenRequester;
 
