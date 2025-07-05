92dde6379a6237fadfa60473c05f1cfa2e428a31
pixlze <henrygoals2007@gmail.com>
2025-07-04 23:58:55 -0400

    refactor: update authentication for v3

--- ogybot.Communication/Requests/GetTokenRequest.cs
+++ ogybot.Communication/Requests/GetTokenRequest.cs
@@ -1,3 +1,4 @@
 ﻿namespace ogybot.Communication.Requests;
 
-public record GetTokenRequest(string ValidationKey);
+public record GetTokenRequest(string Code, string Grant_Type = "authorization_code", string McUsername = "!bot");
+public record RefreshTokenRequest(string RefreshToken, string Grant_Type = "refresh_token");
